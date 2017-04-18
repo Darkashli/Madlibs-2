c21959b100207744c6714280694ea3a7f38b0667
Mhd Alaa Darkashli <99034433@mydavinci.nl>
2017-04-18 10:50:24 +0200

    First step

---
+++ actionpage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Madlibs</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <meta charset="utf-8">
+</head>
+<body>
+  
+  <header>mad Libs</header>
+    
+    <nav>
+      <ul>
+        <li>Er heerst paniek...</li>
+        <li>Onkunde</li>
+      </ul>
+    </nav>
+
+<div class="content">
+
+
+Er heerst paniek in het koninkrijk <?php echo $_POST['v1']; ?>. Koning <?php echo $_POST['v2']; ?> is ten einde raad en als koning <?php echo $_POST['v3']; ?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?php echo $_POST['v4']; ?>.
+
+"<?php echo $_POST['v5']; ?>! Het is een ramp! Het is een schande!"  "Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"
+"Mijn <?php echo $_POST['v6']; ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net  <?php echo $_POST['v7']; ?> voor hem gekocht!"
+"Majesteit, uw <?php echo $_POST['v8']; ?> komt vast vanzelf weer terug?"
+"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST['v1']; ?> leren?"
+"Maar Sire, daar kunt u toch uw <?php echo $_POST['v2']; ?> voor gebruiken."
+"<?php echo $_POST['v3']; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."
+"<?php echo $_POST['v4']; ?>, Sire."
+
+
+
+
+<footer>&copy;<?php echo date("Y");?></footer>
+</div>
+
+</body>
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Madlibs</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <meta charset="utf-8">
+</head>
+<body>
+    
+  
+    <header>mad Libs</header>
+
+    <nav>
+       <ul>
+         <li>Er heerst paniek...</li>
+         <li>Onkunde</li>
+       </ul>
+      </nav>
+    <div class="content">
+    <form method="POST" action="actionpage.php">
+         <h3>Er heerst paniek...</h3><br>
+	       <span>Welke dier zou je nooit als huisdier willen hebben? <input type="text" name="v1"></span><br>
+         <span>Wie is de belangrijkste persoon in je leven? <input type="text" name="v2"></span><br>
+         <span>In welk land zou je graag willen wonen? <input type="text" name="v3"></span><br>
+         <span>Wat doe je als je je verveelt? <input type="text" name="v4"></span><br>
+         <span>Met welk speelgoed speelde je als kind het meest? <input type="text" name="v5"></span><br>
+         <span>Bij welke docent spijbel je het liefst? <input type="text" name="v6"></span><br>
+         <span>Als je €100.000,-had, wat zou je dan kopen? <input type="text" name="v7"></span><br>
+         <span>Wat is je favoriete bezigheid? <input type="text" name="v8"></span><br>
+
+         <input class="button" type="submit"  value="Versturen">
+         </form>
+         <footer>2017-2018</footer>
+      </div>
+  </body>
+</html>
